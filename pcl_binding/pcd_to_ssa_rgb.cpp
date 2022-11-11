2cd0d8154f848df97150a375437aeb54a3a30250
Yeah2333 <653091941@qq.com>
2022-11-11 13:37:50 +0800

    init commit with ubuntu 18.04

--- pcl_binding/pcd_to_ssa_rgb.cpp
+++ pcl_binding/pcd_to_ssa_rgb.cpp
@@ -27,105 +27,99 @@
 using namespace std;
 using namespace ssa;
 
-const char *message[]={
-  "pcd_to_ssa_rgb: converts a set of PointXYZRGB pcd files into a ssa 3d graph file",
-  "usaage: pcd_to_ssa_rgb [options] <ssa3d_file>",
-  "options:",
-  "-e [int]            take every e.th scan.",
-  "-n [int]            number of scans.",
-  "-p [string]         prefix of the pcd file list.",
-  "-lms                use lms sensor model instead of kinect.",
-  "-r [double]         resolution of the resulting model (default resolution 0.01m)",
-  "example:",
-  "pcd_to_ssa_rgb -p alufr_black_mug_raw -n 60 -r 0.001 alufr_black_mug_raw.ssa3d",
-  0
+const char *message[] = {
+        "pcd_to_ssa_rgb: converts a set of PointXYZRGB pcd files into a ssa 3d graph file",
+        "usaage: pcd_to_ssa_rgb [options] <ssa3d_file>",
+        "options:",
+        "-e [int]            take every e.th scan.",
+        "-n [int]            number of scans.",
+        "-p [string]         prefix of the pcd file list.",
+        "-lms                use lms sensor model instead of kinect.",
+        "-r [double]         resolution of the resulting model (default resolution 0.01m)",
+        "example:",
+        "pcd_to_ssa_rgb -p alufr_black_mug_raw -n 60 -r 0.001 alufr_black_mug_raw.ssa3d",
+        0
 };
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
- //Debuging stuff
- //OptimizableGraph::printRegisteredTypes(cout, true);
-  string pcPrefix  = "pcloud";
- if (argc<2){
-    const char**v=message;
-    while (*v){
-      cout << *v << endl;
-      v++;
+    //Debuging stuff
+    //OptimizableGraph::printRegisteredTypes(cout, true);
+    string pcPrefix = "pcloud";
+    if (argc < 2) {
+        const char **v = message;
+        while (*v) {
+            cout << *v << endl;
+            v++;
+        }
+        return 0;
     }
-    return 0; 
-  }
 
-  const char* outputFile=0;
+    const char *outputFile = 0;
 
-  int numOfScans = 100;
-  int every = 1;
-  double voxelSize = 0.01;
-  bool useLMS = false;
-  int c=1;
-  while (c<argc){
-    if (!strcmp(argv[c],"-e")){
-      c++;
-      every=atoi(argv[c]);
-      c++;
-    } else 
-    if (!strcmp(argv[c],"-n")){
-      c++;
-      numOfScans=atoi(argv[c]);
-      c++;
-    } else 
-    if (!strcmp(argv[c],"-p")){
-      c++;
-      pcPrefix=argv[c];
-      c++;
-    } else 
-    if (!strcmp(argv[c],"-r")){
-      c++;
-      voxelSize=atof(argv[c]);
-      c++;
-    } else
-    if (!strcmp(argv[c],"-lms")){
-      useLMS = true;
-      c++;
-    } else
-    if (! outputFile){
-      outputFile=argv[c];
-      c++;
-      break;
+    int numOfScans = 100;
+    int every = 1;
+    double voxelSize = 0.01;
+    bool useLMS = false;
+    int c = 1;
+    while (c < argc) {
+        if (!strcmp(argv[c], "-e")) {
+            c++;
+            every = atoi(argv[c]);
+            c++;
+        } else if (!strcmp(argv[c], "-n")) {
+            c++;
+            numOfScans = atoi(argv[c]);
+            c++;
+        } else if (!strcmp(argv[c], "-p")) {
+            c++;
+            pcPrefix = argv[c];
+            c++;
+        } else if (!strcmp(argv[c], "-r")) {
+            c++;
+            voxelSize = atof(argv[c]);
+            c++;
+        } else if (!strcmp(argv[c], "-lms")) {
+            useLMS = true;
+            c++;
+        } else if (!outputFile) {
+            outputFile = argv[c];
+            c++;
+            break;
+        }
     }
-  }
-  //Create ssa 3d graph
-  SparseSurfaceAdjustmentGraph3D ssaGraph;
+    //Create ssa 3d graph
+    SparseSurfaceAdjustmentGraph3D ssaGraph;
 
-  vector< pcl::PointCloud<pcl::PointXYZRGB>::Ptr > clouds;
-  cerr << "Building ssa graph with resolution " << voxelSize << "m."<< endl;
+    vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clouds;
+    cerr << "Building ssa graph with resolution " << voxelSize << "m." << endl;
 
-  PCLSSAHierarchicalT<pcl::PointCloud<pcl::PointXYZRGB> >  pclToSSA;
-  //pclToSSA.setInput(&clouds);
-  pclToSSA.setSensor(PCLSSAHierarchicalT<pcl::PointCloud<pcl::PointXYZRGB> >::KINECT);
-  pclToSSA.setLevels(5);
-  pclToSSA.setResolution(0, 0.001);
-  pclToSSA.setResolution(1, 0.005);
-  pclToSSA.setResolution(2, 0.01);
-  pclToSSA.setResolution(3, 0.02);
-  pclToSSA.setResolution(4, 0.05);
-  pclToSSA.params().normalExtractionMaxNeighborDistance = 0.2;
-  pclToSSA.params().normalExtractionMinNeighbors = 9;
-  //pclToSSA.constructGraph(ssaGraph);
+    PCLSSAHierarchicalT<pcl::PointCloud<pcl::PointXYZRGB> > pclToSSA;
+    //pclToSSA.setInput(&clouds);
+    pclToSSA.setSensor(PCLSSAHierarchicalT<pcl::PointCloud<pcl::PointXYZRGB> >::KINECT);
+    pclToSSA.setLevels(5);
+    pclToSSA.setResolution(0, 0.001);
+    pclToSSA.setResolution(1, 0.005);
+    pclToSSA.setResolution(2, 0.01);
+    pclToSSA.setResolution(3, 0.02);
+    pclToSSA.setResolution(4, 0.05);
+    pclToSSA.params().normalExtractionMaxNeighborDistance = 0.2;
+    pclToSSA.params().normalExtractionMinNeighbors = 9;
+    //pclToSSA.constructGraph(ssaGraph);
 
-  for(int i = 0; i < numOfScans; i=i+every)
-  {
-    double timing = get_time();
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-    char cloudFileName[2048];
-    sprintf(cloudFileName, "%s%05d.pcd", pcPrefix.c_str(), i);
-    pcl::io::loadPCDFile(cloudFileName , *cloud);
-    cerr << "loading from disk took " << (get_time() - timing) * 1000 << "ms." << endl;
-    pclToSSA.addCloudToGraph(ssaGraph, cloud);
-  }
+    for (int i = 0; i < numOfScans; i = i + every) {
+        double timing = get_time();
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+        char cloudFileName[2048];
+        sprintf(cloudFileName, "%s%05d.pcd", pcPrefix.c_str(), i);
+        pcl::io::loadPCDFile(cloudFileName, *cloud);
+        cerr << "loading from disk took " << (get_time() - timing) * 1000 << "ms." << endl;
+        pclToSSA.addCloudToGraph(ssaGraph, cloud);
+    }
 
-  cout << "Writing ssa3d file to " << outputFile << endl;
-  ssaGraph.save(outputFile);
+    cout << "Writing ssa3d file to " << outputFile << endl;
+    ssaGraph.save(outputFile);
+    return 0;
 }
 
--- ssa/core/sparse_surface_adjustment.hpp
+++ ssa/core/sparse_surface_adjustment.hpp
@@ -14,207 +14,200 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace ssa{
+namespace ssa {
 
-  template <typename EdgeType1, typename EdgeType2, typename EdgeType3>
-  SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::SparseSurfaceAdjustmentT()
-  {
+    template<typename EdgeType1, typename EdgeType2, typename EdgeType3>
+    SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::SparseSurfaceAdjustmentT() {
 
-  }
+    }
 
-  template <typename EdgeType1, typename EdgeType2, typename EdgeType3>
-  SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::~SparseSurfaceAdjustmentT()
-  {
+    template<typename EdgeType1, typename EdgeType2, typename EdgeType3>
+    SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::~SparseSurfaceAdjustmentT() {
 
-  }
+    }
 
-  template <typename EdgeType1, typename EdgeType2, typename EdgeType3>
-  void
-  SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::setGraph(SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::SSAGraph& graph)
-  {
-    graph_ = graph;
-  }
+    template<typename EdgeType1, typename EdgeType2, typename EdgeType3>
+    void
+    SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::setGraph(
+            SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::SSAGraph &graph) {
+        graph_ = graph;
+    }
 
-  template <typename EdgeType1, typename EdgeType2, typename EdgeType3>
-  typename SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::SSAGraph*
-  SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::graph()
-  {
-    return &graph_;
-  }
+    template<typename EdgeType1, typename EdgeType2, typename EdgeType3>
+    typename SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::SSAGraph *
+    SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::graph() {
+        return &graph_;
+    }
 
-  template <typename EdgeType1, typename EdgeType2, typename EdgeType3>
-  void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::setSolver(g2o::BlockSolverX::LinearSolverType*& linearSolver)
-  {
-    g2o::BlockSolver< BlockSolverTraits<-1, -1> >* blockSolver = new g2o::BlockSolver< BlockSolverTraits<-1, -1> >(linearSolver);
-    g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg(blockSolver);
-    graph_._optimizer.setAlgorithm(solver);
-  }
+    template<typename EdgeType1, typename EdgeType2, typename EdgeType3>
+    void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::setSolver(
+            g2o::BlockSolverX::LinearSolverType *&linearSolver) {
+        g2o::BlockSolver<BlockSolverTraits<-1, -1> > *blockSolver = new g2o::BlockSolver<BlockSolverTraits<-1, -1> >(
+                linearSolver);
+        g2o::OptimizationAlgorithmLevenberg *solver = new g2o::OptimizationAlgorithmLevenberg(blockSolver);
+        graph_._optimizer.setAlgorithm(solver);
+    }
 
-  template <typename EdgeType1, typename EdgeType2, typename EdgeType3>
-  void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::setVerbose(bool verbose)
-  {
-    graph_._optimizer.setVerbose(verbose);
-    verbose_ = verbose;
-  }
+    template<typename EdgeType1, typename EdgeType2, typename EdgeType3>
+    void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::setVerbose(bool verbose) {
+        graph_._optimizer.setVerbose(verbose);
+        verbose_ = verbose;
+    }
 
-  template <typename EdgeType1, typename EdgeType2, typename EdgeType3>
-  void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::setParams(SparseSurfaceAdjustmentParams& params)
-  {
-    params_ = params;
-  }
+    template<typename EdgeType1, typename EdgeType2, typename EdgeType3>
+    void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::setParams(SparseSurfaceAdjustmentParams &params) {
+        params_ = params;
+    }
 
-  template <typename EdgeType1, typename EdgeType2, typename EdgeType3>
-  SparseSurfaceAdjustmentParams& SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::params()
-  {
-    return params_;
-  }
+    template<typename EdgeType1, typename EdgeType2, typename EdgeType3>
+    SparseSurfaceAdjustmentParams &SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::params() {
+        return params_;
+    }
 
-  template <typename EdgeType1, typename EdgeType2, typename EdgeType3>
-  void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::optimizeHierarchical(int startLevel)
-  {
-    for(int i = startLevel; i >= 0; --i)
-      optimize(i);
-  }
+    template<typename EdgeType1, typename EdgeType2, typename EdgeType3>
+    void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::optimizeHierarchical(int startLevel) {
+        for (int i = startLevel; i >= 0; --i)
+            optimize(i);
+    }
 
-  template <typename EdgeType1, typename EdgeType2, typename EdgeType3>
-  void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::optimize(int level)
-  {
-    double timing;
-    double startTime = get_time();
-    std::map<int, int> keys = graph_.getScanIds();
+    template<typename EdgeType1, typename EdgeType2, typename EdgeType3>
+    void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::optimize(int level) {
+        double timing;
+        double startTime = get_time();
+        std::map<int, int> keys = graph_.getScanIds();
 
-    for(int i = 0; i < params().ssaIterations; ++i){
-      double iterationTime = get_time();
-      cerr << "Iteration " << i+1 << endl;
+        for (int i = 0; i < params().ssaIterations; ++i) {
+            double iterationTime = get_time();
+            cerr << "Iteration " << i + 1 << endl;
 
-      //! Update point covariances and normals
-      timing = get_time();
-      graph_.calcMeanCov(params());
-      cerr << "Updating observation covariances took " << (get_time() - timing) * 1000 << " ms" << endl;
+            //! Update point covariances and normals
+            timing = get_time();
+            graph_.calcMeanCov(params());
+            cerr << "Updating observation covariances took " << (get_time() - timing) * 1000 << " ms" << endl;
 
-      /// Estimate data association
-      timing = get_time();
-        graph_.dropDataAssociation();
-      /// use new abstract data association
-      if(params().nearestNeighbor.maxSearchDistance > 0.0){
-        DataAssociationT<EdgeType1, EdgeType2, EdgeType3> da_kdtree;
+            /// Estimate data association
+            timing = get_time();
+            graph_.dropDataAssociation();
+            /// use new abstract data association
+            if (params().nearestNeighbor.maxSearchDistance > 0.0) {
+                DataAssociationT<EdgeType1, EdgeType2, EdgeType3> da_kdtree;
 
-	da_kdtree.setStrategy(DataAssociationT<EdgeType1, EdgeType2, EdgeType3>::KDTREE);
-        da_kdtree.apply(graph_, params(), level);
-      }
+                da_kdtree.setStrategy(DataAssociationT<EdgeType1, EdgeType2, EdgeType3>::KDTREE);
+                da_kdtree.apply(graph_, params(), level);
+            }
 
 //         da_kdtree.setStrategy(DataAssociationT<EdgeType1, EdgeType2, EdgeType3>::KDTREE_REP_SUBSET);
 //         da_kdtree.apply(graph_, params(), level);
 
-	if(params().normalShooting.maxSearchDistance > 0.0)
-	  NormalShootingFlann<EdgeType1, EdgeType2, EdgeType3>::shootNormals(graph_, params().normalShooting);
+            if (params().normalShooting.maxSearchDistance > 0.0)
+                NormalShootingFlann<EdgeType1, EdgeType2, EdgeType3>::shootNormals(graph_, params().normalShooting);
 
-        cerr << "Data association took " << (get_time() - timing) * 1000 << " ms" << endl;
+            cerr << "Data association took " << (get_time() - timing) * 1000 << " ms" << endl;
 
-      /// get edge set
-      timing = get_time();
-      g2o::OptimizableGraph::EdgeSet eset = graph_.getEdgesetFast();
-      cerr << "Edge set construction of size " << eset.size() << " took " << (get_time() - timing) * 1000 << " ms" << endl;
-      ///avoid optimizer segfault
-      if(eset.size() == 0)
-       continue;
+            /// get edge set
+            timing = get_time();
+            g2o::OptimizableGraph::EdgeSet eset = graph_.getEdgesetFast();
+            cerr << "Edge set construction of size " << eset.size() << " took " << (get_time() - timing) * 1000 << " ms"
+                 << endl;
+            ///avoid optimizer segfault
+            if (eset.size() == 0)
+                continue;
 
-      /// initializeOptimization
-      timing = get_time();
-      graph_._optimizer.initializeOptimization(eset);
-      cerr << "initializeOptimization took " << (get_time() - timing) * 1000 << " ms" << endl;
+            /// initializeOptimization
+            timing = get_time();
+            graph_._optimizer.initializeOptimization(eset);
+            cerr << "initializeOptimization took " << (get_time() - timing) * 1000 << " ms" << endl;
 
-      timing = get_time();
-      if(level == 0){
-	graph_._optimizer.optimize(params().g2oIterations);
-      } else {
-	graph_._optimizer.optimize(1);
-      }
-      cerr << "g2o took " << (get_time() - timing) * 1000 << " ms" << endl;
+            timing = get_time();
+            if (level == 0) {
+                graph_._optimizer.optimize(params().g2oIterations);
+            } else {
+                graph_._optimizer.optimize(1);
+            }
+            cerr << "g2o took " << (get_time() - timing) * 1000 << " ms" << endl;
 
-      /// moveUnoptimizedPoints
-      timing = get_time();
-      graph_.moveUnoptimizedPoints();
-      cerr << "moveUnoptimizedPoints took " << (get_time() - timing) * 1000 << " ms" << endl;
+            /// moveUnoptimizedPoints
+            timing = get_time();
+            graph_.moveUnoptimizedPoints();
+            cerr << "moveUnoptimizedPoints took " << (get_time() - timing) * 1000 << " ms" << endl;
 
-      cerr << "Iteration took " << (get_time() - iterationTime) * 1000 << " ms" << endl;
-    }
-    if(params().outlierRejectionMinConnectedNeighbors > 1){
-      graph()->filterOutlier(params().outlierRejectionMinConnectedNeighbors);
-    }
-    graph_.dropDataAssociation();
-    cerr << "Computation took " << (get_time() - startTime) * 1000 << " ms" << endl;
-  }
-
-  template <typename EdgeType1, typename EdgeType2, typename EdgeType3>
-  void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::dumpConnectivityMatrix()
-  {
-    ofstream matrix("ssa-matrix-dump.txt");
-      for (g2o::OptimizableGraph::EdgeSet::iterator it=graph()->_optimizer.edges().begin(); it!=graph()->_optimizer.edges().end(); it++)
-      {
-        matrix << (*it)->vertices()[0]->id() << " " << (*it)->vertices()[1]->id() << " 1" << endl;
-      }
-    matrix.close();
-  }
-
-  template <typename EdgeType1, typename EdgeType2, typename EdgeType3>
-  void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::optimizeColorsIncidenteWeight()
-  {
-    for(int i = 0; i < (int) graph()->_verticies_points.size(); ++i){
-      PointVertex* point = graph()->_verticies_points[i];
-      std::vector<PointVertex* > neighborhood;
-      double totalWeight = 0;
-      double r = 0; //point->cr;
-      double g = 0; //point->cg;
-      double b = 0; //point->cb;
-
-      //find neighborhood
-      for (g2o::OptimizableGraph::EdgeSet::iterator it=point->edges().begin(); it!=point->edges().end(); it++){
-        EdgeType3* edge=dynamic_cast<EdgeType3*>(*it);
-        if(edge){
-          PointVertex* vp1=dynamic_cast<PointVertex*>(edge->vertices()[0]);
-          PointVertex* vp2=dynamic_cast<PointVertex*>(edge->vertices()[1]);
-          if(point->id() != vp1->id()){
-            neighborhood.push_back(vp1);
-          }
-          if(point->id() != vp2->id()){
-            neighborhood.push_back(vp2);
-          }
+            cerr << "Iteration took " << (get_time() - iterationTime) * 1000 << " ms" << endl;
         }
-      }
+        if (params().outlierRejectionMinConnectedNeighbors > 1) {
+            graph()->filterOutlier(params().outlierRejectionMinConnectedNeighbors);
+        }
+        graph_.dropDataAssociation();
+        cerr << "Computation took " << (get_time() - startTime) * 1000 << " ms" << endl;
+    }
 
-      ///Add weighted color values
-      for(size_t j = 0; j < neighborhood.size(); ++j){
-        PointVertex*& n = neighborhood[j];
-        ///find sensor edge and calculate incidence angle
-        PoseVertex* nParent =  n->parentVertex();
-        PointVector beam = nParent->estimate().translation() - n->estimate();
-        double incidenceAngle = fabs(acos(beam.dot(n->normal())));
-        double weight = 1.0; // - (incidenceAngle / (M_PI * 0.5));
+    template<typename EdgeType1, typename EdgeType2, typename EdgeType3>
+    void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::dumpConnectivityMatrix() {
+        ofstream matrix("ssa-matrix-dump.txt");
+        for (g2o::OptimizableGraph::EdgeSet::iterator it = graph()->_optimizer.edges().begin();
+             it != graph()->_optimizer.edges().end(); it++) {
+            matrix << (*it)->vertices()[0]->id() << " " << (*it)->vertices()[1]->id() << " 1" << endl;
+        }
+        matrix.close();
+    }
+
+    template<typename EdgeType1, typename EdgeType2, typename EdgeType3>
+    void SparseSurfaceAdjustmentT<EdgeType1, EdgeType2, EdgeType3>::optimizeColorsIncidenteWeight() {
+        for (int i = 0; i < (int) graph()->_verticies_points.size(); ++i) {
+            PointVertex *point = graph()->_verticies_points[i];
+            std::vector<PointVertex *> neighborhood;
+            double totalWeight = 0;
+            double r = 0; //point->cr;
+            double g = 0; //point->cg;
+            double b = 0; //point->cb;
+
+            //find neighborhood
+            for (g2o::OptimizableGraph::EdgeSet::iterator it = point->edges().begin();
+                 it != point->edges().end(); it++) {
+                EdgeType3 *edge = dynamic_cast<EdgeType3 *>(*it);
+                if (edge) {
+                    PointVertex *vp1 = dynamic_cast<PointVertex *>(edge->vertices()[0]);
+                    PointVertex *vp2 = dynamic_cast<PointVertex *>(edge->vertices()[1]);
+                    if (point->id() != vp1->id()) {
+                        neighborhood.push_back(vp1);
+                    }
+                    if (point->id() != vp2->id()) {
+                        neighborhood.push_back(vp2);
+                    }
+                }
+            }
+
+            ///Add weighted color values
+            for (size_t j = 0; j < neighborhood.size(); ++j) {
+                PointVertex *&n = neighborhood[j];
+                ///find sensor edge and calculate incidence angle
+                PoseVertex *nParent = n->parentVertex();
+                PointVector beam = nParent->estimate().translation() - n->estimate();
+                double incidenceAngle = fabs(acos(beam.dot(n->normal())));
+                double weight = 1.0; // - (incidenceAngle / (M_PI * 0.5));
 //         weight = min(weight, 0.0);
 //         weight *= weight;
 //         if(RAD2DEG(incidenceAngle) > 60.0)
 //           weight = 0.0;
-        r += n->cr * weight;
-        g += n->cg * weight;
-        b += n->cb * weight;
-        totalWeight += weight;
-      }
+                r += n->cr * weight;
+                g += n->cg * weight;
+                b += n->cb * weight;
+                totalWeight += weight;
+            }
 
-      //set colors
-      if(neighborhood.size() > 1){
-        point->cr = r / totalWeight;
-        point->cg = g / totalWeight;
-        point->cb = b / totalWeight;
-        for(size_t j = 0; j < neighborhood.size(); ++j){
-          PointVertex*& n = neighborhood[j];
-          n->cr = point->cr;
-          n->cg = point->cg;
-          n->cb = point->cb;
+            //set colors
+            if (neighborhood.size() > 1) {
+                point->cr = r / totalWeight;
+                point->cg = g / totalWeight;
+                point->cb = b / totalWeight;
+                for (size_t j = 0; j < neighborhood.size(); ++j) {
+                    PointVertex *&n = neighborhood[j];
+                    n->cr = point->cr;
+                    n->cg = point->cg;
+                    n->cb = point->cb;
+                }
+            }
         }
-      }
     }
-  }
 
 } //end namespace
 
